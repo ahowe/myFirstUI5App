e66f7516fb6f04a5237145e92e49d171582e2154
Howe Andre <andreh@pacbell.net>
2019-10-28 20:39:03 +0000

    Initial Commit

---
+++ webapp/controller/App.controller.js
@@ -0,0 +1,15 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller",
+	"demo/ui5/myFirstUI5App/externalURL/externalURL"
+], function (Controller,oURL) {
+	"use strict";
+
+	return Controller.extend("demo.ui5.myFirstUI5App.controller.App", {
+		onInit: function () {
+			
+			debugger;
+			var oModel = new sap.ui.model.json.JSONModel(oURL.jsonDemo);   
+			this.getView().setModel(oModel,"demoView");
+		}
+	});
+});
